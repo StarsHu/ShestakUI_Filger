d5b7c58eb4356aa8e097a4739ad3179a0a8834fd
Stars Hu <hxldxx99@sina.com>
2016-08-08 18:18:03 +0800

    Update move mode

--- ShestakUI_Filger.lua
+++ ShestakUI_Filger.lua
@@ -7,6 +7,7 @@ local Misc = ns.Misc
 
 local class = select(2, UnitClass("player"))
 local ClassColor = RAID_CLASS_COLORS[class]
+local TestMode = Misc.testMode
 
 local colorTable = {
 	["DK"]		= {.77, .12, .23, 1},	-- DeathKnight	196,31,59
@@ -575,3 +576,82 @@ if Filger_Spells and Filger_Spells[class] then
 		frame.movebar = movebar
 	end
 end
+
+-- TestMode module
+if TestMode == true then
+    for i = 1, #Filger_Spells[class], 1 do
+        local data = Filger_Spells[class][i]
+        local frame = _G["FilgerFrame"..i.."_"..data.Name]
+        frame.actives = {}
+--        if testMode then
+        for j = 1, math.min(Misc.maxTestIcon, #Filger_Spells[class][i]), 1 do
+            local data = Filger_Spells[class][i][j]
+            local name, icon
+            if data.spellID then
+                name, _, icon = GetSpellInfo(data.spellID)
+            elseif data.slotID then
+                local slotLink = GetInventoryItemLink("player", data.slotID)
+                if slotLink then
+                    name, _, _, _, _, _, _, _, _, icon = GetItemInfo(slotLink)
+                end
+            end
+            frame.actives[j] = {data = data, name = name, icon = icon, count = 9, start = 0, duration = 0, spid = data.spellID or data.slotID}
+        end
+--            frame:SetScript("OnEvent", nil)
+--            frame.movebar:Show()
+--        else
+--            frame:SetScript("OnEvent", Filger.OnEvent)
+--            frame.movebar:Hide()
+--        end
+        Filger.DisplayActives(frame)
+    end
+end
+
+-- Commands module
+local FilgerCommand = function(msg)
+	msg = string.lower(msg)
+	if msg == "test" then
+		if UnitAffectingCombat("player") then print("|cffffff00"..ERR_NOT_IN_COMBAT.."|r") return end
+		TestMode = not TestMode
+--		for i = 1, #Filger_Spells[class], 1 do
+--			local data = Filger_Spells[class][i]
+--			local frame = _G["FilgerFrame"..i.."_"..data.Name]
+--			frame.actives = {}
+--			if testMode then
+--				for j = 1, math.min(Misc.maxTestIcon, #Filger_Spells[class][i]), 1 do
+--					local data = Filger_Spells[class][i][j]
+--					local name, icon
+--					if data.spellID then
+--						name, _, icon = GetSpellInfo(data.spellID)
+--					elseif data.slotID then
+--						local slotLink = GetInventoryItemLink("player", data.slotID)
+--						if slotLink then
+--							name, _, _, _, _, _, _, _, _, icon = GetItemInfo(slotLink)
+--						end
+--					end
+--					frame.actives[j] = {data = data, name = name, icon = icon, count = 9, start = 0, duration = 0, spid = data.spellID or data.slotID}
+--				end
+--				frame:SetScript("OnEvent", nil)
+--				frame.movebar:Show()
+--			else
+--				frame:SetScript("OnEvent", Filger.OnEvent)
+--				frame.movebar:Hide()
+--			end
+--			Filger.DisplayActives(frame)
+--		end
+	elseif msg == "reset" then
+		for i = 1, #Filger_Spells[class], 1 do
+			local data = Filger_Spells[class][i]
+			local frame = _G["FilgerFrame"..i.."_"..data.Name]
+			frame.movebar:ClearAllPoints()
+			frame.movebar:SetPoint(unpack(data.Position))
+		end
+	else
+		print("|cffffff00/Filger test   解锁/锁定|r")
+		print("|cffffff00/Filger reset  重置|r")
+	end
+end
+
+SlashCmdList.FilgerTest = FilgerCommand
+SLASH_FilgerTest1 = "/sf"
+SLASH_FilgerTest2 = "/filger"
